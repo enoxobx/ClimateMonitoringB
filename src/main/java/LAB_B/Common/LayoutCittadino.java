20d9b5e45949d6e455ae4768b0a6dfa76551ea89
enoxobx <dushi98@hotmail.it>
2024-12-10 16:52:52 +0100

    deleted:    .gradle/8.8/checksums/checksums.lock 	deleted:    .gradle/8.8/checksums/md5-checksums.bin 	deleted:    .gradle/8.8/checksums/sha1-checksums.bin 	deleted:    .gradle/8.8/dependencies-accessors/gc.properties 	deleted:    .gradle/8.8/fileChanges/last-build.bin 	deleted:    .gradle/8.8/fileHashes/fileHashes.lock 	deleted:    .gradle/8.8/gc.properties 	deleted:    .gradle/buildOutputCleanup/buildOutputCleanup.lock 	deleted:    .gradle/buildOutputCleanup/cache.properties 	deleted:    .gradle/vcs-1/gc.properties 	renamed:    insert.txt -> insert.sql 	modified:   pom.xml 	new file:   src/main/java/LAB_B/Common/LayoutCittadino.java 	modified:   table.sql

---
+++ src/main/java/LAB_B/Common/LayoutCittadino.java
@@ -0,0 +1,17 @@
+import com.bbn.openmap.ApplicationContainer;
+import com.bbn.openmap.MapHandler;
+import com.bbn.openmap.gui.OpenMapApp;
+import com.bbn.openmap.proj.coords.LatLonPoint;
+import com.bbn.openmap.tools.drawing.DrawingTool;
+import com.bbn.openmap.tools.drawing.OvalTool;
+
+import javax.swing.*;
+import java.awt.*;
+
+package LAB_B.Common;
+
+public class LayoutCittadino extends LayoutStandard {
+
+
+    
+}
